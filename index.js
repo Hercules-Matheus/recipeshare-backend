32cefb3c8277bb806f39aef4c20c043600590547
Hercules <hmls235@gmail.com>
2024-12-02 21:40:15 -0300

    fix: fix font import bug CSP

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
     "default-src 'self'; " +
       "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
       "style-src 'self' 'unsafe-inline'; " +
-      "font-src 'self';"
+      "font-src 'self' https://fonts.gstatic.com;"
   );
   next();
 });
