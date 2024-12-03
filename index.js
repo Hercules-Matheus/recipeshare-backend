96062313a506e580aab967c99ce16b780de0dba7
Hercules Matheus Lima Silva <hercules.silva@parnamirim.rn>
2024-12-03 12:21:30 -0300

    update CORS access

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 // Rota OPTIONS para CORS
 app.options("*", cors());
 
